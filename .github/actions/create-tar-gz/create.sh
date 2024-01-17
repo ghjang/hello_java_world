ec5684ee597338692640a863a4431eef07cc41cb
ghjang <jmxexpert@hotmail.com>
2024-01-17 15:56:12 +0900

    create-tar-gz 커스텀 액션에 생성할 tar.gz 파일 경로를 인자로 전달해 활용하게 수정, #7

--- .github/actions/create-tar-gz/create.sh
+++ .github/actions/create-tar-gz/create.sh
@@ -1,3 +1,11 @@
+#!/bin/sh
+
+# Check if BUILD_OUTPUT_FILE is set
+if [ -z "${BUILD_OUTPUT_FILE}" ]; then
+  echo "Error: BUILD_OUTPUT_FILE is not set."
+  exit 1
+fi
+
 cd projects
 
 echo "pwd: $(pwd)"
@@ -12,7 +20,7 @@ echo "hello_world_console_app/target/lib :"
 ls -l hello_world_console_app/target/lib
 
 cd hello_world_console_app/target
-tar -czvf ../../../hello_world_java-${TAG_VERSION}.tar.gz hello_world_console_app*.jar lib
+tar -czvf ../../../${BUILD_OUTPUT_FILE} hello_world_console_app*.jar lib
 
 cd ../../..
 ls -l *.tar.gz
